e131b86ed924bc2fa9063dfd06b390736feb621a
pablocru <pablocru@outlook.com>
2024-04-28 12:21:13 +0200

    feat: Transpile functions always returns a string even if there is an error

--- src/lib/transpile-to-es5.ts
+++ src/lib/transpile-to-es5.ts
@@ -1,17 +1,20 @@
 import { transform } from "@babel/standalone";
 
+const TRANSPILE_ERROR_MESSAGE = "Unable to transpile this code";
+
 /**
  * @param {string} inputCode
  */
-export function transpileToES5(inputCode: string): string | undefined {
+export function transpileToES5(inputCode: string): string {
   try {
     const transpiledCode = transform(inputCode, { presets: ["env"] }).code;
 
     if (typeof transpiledCode !== "string")
-      throw new Error("Unable to transpile this code");
+      throw new Error(TRANSPILE_ERROR_MESSAGE);
 
     return transpiledCode;
   } catch (error) {
     console.error(error);
+    return error instanceof Error ? error.message : TRANSPILE_ERROR_MESSAGE;
   }
 }
